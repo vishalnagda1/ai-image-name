0ed0114565c5c058111446fdafe45cde0ba763a3
Vishal Nagda <vishalnagda1@users.noreply.github.com>
2025-02-19 19:31:43 +0530

    make image name generator class singleton class

--- ai_image_name/index.py
+++ ai_image_name/index.py
@@ -2,8 +2,27 @@ from ollama import Client
 
 
 class ImageNameGenerator:
-    def __init__(self, model: str = "llava:7b", max_token: int = 30):
-        self.client = Client(host="http://0.0.0.0:11434")
+    _instance = None
+
+    def __new__(
+        cls,
+        host: str = "http://0.0.0.0:11434",
+        model: str = "llava:7b",
+        max_token: int = 30,
+    ):
+        if cls._instance is None:
+            cls._instance = super(ImageNameGenerator, cls).__new__(cls)
+            cls._instance.__init__(host, model, max_token)
+        return cls._instance
+
+    def __init__(
+        self,
+        host: str = "http://0.0.0.0:11434",
+        model: str = "llava:7b",
+        max_token: int = 30,
+    ):
+        self.host = host
+        self.client = Client(host=self.host)
         self.model = model
         self.max_token = max_token
 
@@ -15,10 +34,12 @@ class ImageNameGenerator:
             messages=[
                 {
                     "role": "user",
-                    "content": "give a valid name to this image, max length of name should be 30 characters long"
-                    "do not include quotes around the name. The name must not contain any special characters."
-                    "do not include extension in the name and make sure the name is in snake_case"
-                    "for example, if the image is a picture of a cat, the name should be cat_picture",
+                    "content": (
+                        "give a valid name to this image, max length of name should be 30 characters long"
+                        "do not include quotes around the name. The name must not contain any special characters."
+                        "do not include extension in the name and make sure the name is in snake_case"
+                        "for example, if the image is a picture of a cat, the name should be cat_picture",
+                    ),
                     "images": [image_path],
                     "max_token": max_token or self.max_token,
                 },
