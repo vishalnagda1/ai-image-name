f19b3a4fb277b3317fdbc9d471476b2d4a211349
Vishal Nagda <vishalnagda1@users.noreply.github.com>
2025-02-19 19:08:12 +0530

    update prompt

--- main.py
+++ main.py
@@ -11,8 +11,12 @@ def ai_image_name(image_path):
         messages=[
             {
                 "role": "user",
-                "content": "give a valid name to this image, max length of name should be 30 characters long",
-                "images": ["./1.png"],
+                "content": "give a valid name to this image, max length of name should be 30 characters long" \
+                        "do not include quotes around the name. The name must not contain any special characters." \
+                        "do not include extension in the name and make sure the name is in snake_case" \
+                        "for example, if the image is a picture of a cat, the name should be cat_picture",
+                "images": [image_path],
+                "max_token": 30,
             },
         ],
     )
